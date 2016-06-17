8e0a794e0dee7ee580ed5685ddd5be09927db504
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-17 21:10:40 +0900

    MinorFixed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,10 +26,9 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1/edit
   def edit
     # @restaurant = Restaurant.all
-    @restaurant = Restaurant.find(1)
-
-    # ここに入るのはAcctiveRecordなのでレンダーで表示してもからと表示される
-    render :text =>  @restaurant.name
+    # @restaurant = Restaurant.find(2)
+    # @restaurant = Restaurant.where(:user_id => current_user.id)
+    @restaurant = Restaurant.find(current_user.id)
   end
 
   # POST /restaurants
@@ -39,51 +38,6 @@ class RestaurantsController < ApplicationController
     if @restaurant.save
     # スクレイピング先のURL
     restaurant_url = Restaurant.last.url
-
-    ######################################################################
-    # 食べログ
-    ######################################################################
-    # charset = nil
-    # html = open(restaurant_url) do |f|
-    #   charset = f.charset # 文字種別を取得
-    #   f.read # htmlを読み込んで変数htmlに渡す
-    # end
-
-    # # htmlをパース(解析)してオブジェクトを生成
-    # doc = Nokogiri::HTML.parse(html, nil, charset)
-
-    # # スクレイピング先のURL
-    # restaurant_url = Restaurant.last.url
-
-    # # charset = nil
-    # # html = open(restaurant_url) do |f|
-    # #   charset = f.charset # 文字種別を取得
-    # #   f.read # htmlを読み込んで変数htmlに渡す
-    # # end
-
-    # # Heroku用にopen-uriの記述を追加
-    # url_text = Net::HTTP.get(URI.parse restaurant_url)
-    # doc = Nokogiri::HTML(url_text)
-
-    # # htmlをパース(解析)してオブジェクトを生成
-    # # doc = Nokogiri::HTML.parse(html, nil, charset)
-
-
-    # # 店舗名
-    # name =  doc.title
-    # # カテゴリー
-    # category = doc.xpath('//p/span[@property="v:category"][1]/text()').to_s
-    # # 価格帯
-    # price = doc.xpath('//dd[@class="rdhead-budget__price"]/a[@class]/text()').to_s
-    # # price = price[1..price.index("～")-1].delete(",").to_i
-    # # 評価
-    # star = doc.xpath('//strong[@class="score"]/span/text()').to_s
-    # # エリア
-    # area = doc.xpath('//div[@class="parent"]/p/a[1]/text()').first
-    # # イメージ
-    # # image = doc.xpath('//meta[@property="og:image"]').attribute("content").value
-    # image = doc.xpath('//li[@class="mainphoto-box"]/img[@class="mainphoto-image"]').attribute("src").value
-
 
     ######################################################################
     # yelp
@@ -184,11 +138,106 @@ class RestaurantsController < ApplicationController
   # PATCH/PUT /restaurants/1
   # PATCH/PUT /restaurants/1.json
   def update
-    if @restaurant.update(restaurant_params)
 
-      else
-        render 'edit'
+    # パラメータから取得したURLにアクセスしようとしてもレストラン情報の上書きができない
+    # 問題は、URLにアクセスしたときに起こっている。
+
+    url = restaurant_params[:url]
+    comment = restaurant_params[:comment]
+    user_id = restaurant_params[:user_id]
+    payment = restaurant_params[:payment]
+
+    logger.debug(url)
+
+    # スマホURLの対応
+    if (url.index("m") == 8)
+      url = url.gsub("m", "www")
     end
+
+    logger.debug"1"
+
+    opt = {}
+    opt['User-Agent'] = 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01 '
+
+    charset = nil
+    html = open(restaurant_url,opt) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+
+    logger.debug"2"
+
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    logger.debug"3"
+
+    # 店舗名
+    title =  doc.title
+    title_num =  doc.title.index("-")
+    title = title[0..title_num-2]
+    puts title
+
+    # # カテゴリー
+    category = doc.xpath('//span[@class="category-str-list"]/a[1]/text()').to_s
+    # category2 = doc.xpath('//span[@class="category-str-list"]/a[2]/text()').to_s
+    # category3 = doc.xpath('//span[@class="category-str-list"]/a[3]/text()').to_s
+    # puts category1
+    # puts category2
+    # puts category3
+    # 価格帯
+    price = doc.xpath('//dd[@class="nowrap price-description"]/text()').to_s
+    price_head = price.index("￥")
+
+    if price.count("以") > 0
+      price_tail = price.index("以上")
+    elsif price.count("-") > 0
+      price_tail = price.index("-")
+    elsif
+      price_tail = nil
+    end
+
+    if price_tail != nil
+      price = price[price_head+1..price_tail-1].delete(",").to_i
+    end
+
+    puts price
+
+    # 評価
+    star = doc.xpath('//i[@class="star-img stars_4"]').attribute("title").value
+    star_num = star.index("星")
+    star = star[0..star_num-2]
+    puts star
+    # エリア
+    area = doc.xpath('//span[@class="neighborhood-str-list"]/text()')
+    area.index("n")
+    puts area
+    # イメージ
+    image = doc.xpath('//meta[@property="og:image"]').attribute("content").value
+    image_url = doc.xpath('//a[@class="see-more show-all-overlay"]').attribute("href").value
+    image_url = "https://www.yelp.co.jp/" + image_url + "?tab=food"
+
+    html = open(image_url,opt) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    image = doc.xpath('//meta[@property="og:image"]').attribute("content").value
+
+    ######################################################################
+    @restaurant.update_all(
+      :name => title,
+      :category => category,
+      :price => price,
+      :star => star,
+      :area => area,
+      :image => image,
+      :comment => comment,
+      :payment => payment,
+      :url => url,
+    )
+    redirect_to root_path
   end
 
   # DELETE /restaurants/1
