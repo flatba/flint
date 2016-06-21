1d813f26cd497cf557c3ef9d8d614ee0810db858
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-21 22:28:04 +0900

    facebook情報取得の修正

--- app/controllers/flint_controller.rb
+++ app/controllers/flint_controller.rb
@@ -46,8 +46,8 @@ class FlintController < ApplicationController
         else
         @facebook_uid_thumb = "https://graph.facebook.com/"+ facebook_uid +"/picture?type=large"
         end
-        facebook_birthday = User.find(params[:id]).birthday
-        facebook_friends = User.find(params[:id]).friends
+        facebook_birthday = User.find(current_user.id).birthday
+        facebook_friends = User.find(current_user.id).friends
         # facebook_education = User.find(params[:id]).education
         # facebook_work = User.find(params[:id]).work
 
