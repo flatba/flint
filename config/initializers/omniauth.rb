90aeb3c3206160d0025454062211a8e37d3b06d1
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-21 22:51:05 +0900

    Facebook認証修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "id", "pass", {scope: "first_name, gender, public_profile, user_friends, user_birthday"}
+  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_APP_SECRET'], {scope: "first_name, gender, public_profile, user_friends, user_birthday"}
 end
