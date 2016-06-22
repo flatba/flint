b425ba08260111c82d332cd7d28d5b23e11c5661
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-22 13:42:58 +0900

    fix omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_APP_SECRET'], {scope: "public_profile, user_friends, user_birthday"}
+  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_APP_SECRET'], {scope: "public_profile, user_friends, user_birthday", :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" }}}
 end
