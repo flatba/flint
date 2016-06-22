2255aa3de550b0709d237ccf2507a970f053cd62
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-22 20:29:25 +0900

    omniauth.rbの修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "FACEBOOK_APP_ID","FACEBOOK_APP_SECRET ", {:client_options => {:ssl => {:verify => false}}}
+  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_APP_SECRET'], {:client_options => {:ssl => {:verify => false}}}
 end
