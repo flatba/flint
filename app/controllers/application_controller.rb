1d3c670cd75a967cdc04d17743ad864d3ea1889d
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-22 22:57:47 +0900

    delete log.debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
   def sign_in_required
     redirect_to new_user_session_url unless user_signed_in?
-    log.debug "----------------------ここはリダイレクトです---------------------------"
+
   end
 
   # name フィールドを許可する strong parameters の設定
