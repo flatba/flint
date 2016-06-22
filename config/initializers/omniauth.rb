55a626b1dd71324f8effcb44bf329d8da1e46a59
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-22 11:08:20 +0900

    fix omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "id", "pass", {scope: "gender, public_profile, user_friends, user_birthday"}
+  provider :facebook, ENV['FACEBOOK_APP_ID'],ENV['FACEBOOK_APP_SECRET'], {scope: "public_profile, user_friends, user_birthday"}
 end
