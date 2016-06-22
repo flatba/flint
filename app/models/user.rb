2ea06d34c7cc5d718aa2a2224733be16fd4cbe43
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-22 21:00:26 +0900

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,23 +25,40 @@ class User < ActiveRecord::Base
 
   has_many :restaurants
 
-  # self.find_for_oauthを定義
-  # callback後のユーザー登録で、取得した情報を参照する。
+  # Facebookから情報を取得する
   def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
- logger.debug "ここまでで６"
-    unless user
-      user = User.create(
-                         logger.debug "ここまでで７"
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20]
-      )
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.gender = auth.extra.raw_info.gender
+      user.thumb = "https://graph.facebook.com/"+auth.uid.to_s+"/picture?type=large"
+      user.age_range = auth.extra.raw_info.age_range.min.last
+      user.friends = auth.info.user_friends
+      user.birthday = auth.info.user_birthday
+      # user.image = auth.info.image # assuming the user model has an image
+      user.education = auth.info.user_education_history
+      user.work = auth.info.user_work_history
+
+
     end
-logger.debug "ここまでで8"
-    user
   end
+
+# def self.find_for_oauth(auth)
+#     user = User.where(uid: auth.uid, provider: auth.provider).first
+
+#     unless user
+#       user = User.create(
+#         uid:      auth.uid,
+#         provider: auth.provider,
+#         email:    User.dummy_email(auth),
+#         password: Devise.friendly_token[0, 20]
+#       )
+#     end
+
+#     user
+#   end
+
 
   #...
 
@@ -64,7 +81,6 @@ logger.debug "ここまでで8"
   validates :name, :age_range , presence: true, length: { maximum: 50 } # Userテーブルのnameカラム
 
   private
-  # まだuserでない場合はダミーメール作成しユーザー情報を保存
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
