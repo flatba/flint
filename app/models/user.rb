a99a176ce02c03e04c8493247c8564bc929138f3
Yoshiki Hiraba <flatba@YsMac.local>
2016-06-24 21:10:56 +0900

    facebook情報取得の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,10 +36,13 @@ class User < ActiveRecord::Base
       user.thumb = "https://graph.facebook.com/"+auth.uid.to_s+"/picture?type=large"
       user.age_range = auth.extra.raw_info.age_range.min.last
 
-
+      user_friends.uid = auth.extra.raw_info.friends.data.id
+      user_friends.name = auth.extra.raw_info.friends.data.name
+      user_friends.user = auth.uid
+      user.birthday = auth.extra.raw_info.birthday
 
       # @friends = auth.extra.raw_info.friends.data # 中に入るのは"name"と"id"
-      # user.birthday = auth.extra.raw_info.birthday
+
       # user.image = auth.info.image # assuming the user model has an image
       # user.education = auth.user_education_history.name
       # user.work = auth.user_work_history.name
